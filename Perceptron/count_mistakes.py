48c1812c8b7ac40027da5bb29ffee549f2b488a4
Pratham Tandon <prathamt@outlook.com>
2015-12-03 14:12:14 -0800

    Add Report

--- Perceptron/count_mistakes.py
+++ Perceptron/count_mistakes.py
@@ -1,69 +1,261 @@
 #!/usr/bin/python
-from constants import count_mistakes_datasets
+import matplotlib.pyplot as plt
 from datahelper import file_read, partition_data
-from perceptron import perceptron_get_params, perceptron_mistake_count
-from winnow import winnow_get_params, winnow_mistake_count
-from plothelper import plot_mistakes
 
-def count_mistakes_perceptron (n_val, dataset, training, testing):
-    perceptron_params = perceptron_get_params (n_val, training, testing)
-    
-    perceptron_params_without_margin = perceptron_params.get_params(0)
-    perceptron_params_with_margin = perceptron_params.get_params(1)
-    
-    mistakes_without_margin = perceptron_mistake_count (dataset, 
-                                perceptron_params_without_margin)
-    mistakes_with_margin = perceptron_mistake_count (dataset, 
-                                perceptron_params_with_margin)
-    
-    return (mistakes_without_margin, mistakes_with_margin)
+n_val = 1000
+perceptron_parameters = [[[0],[1]],[[1],[1.5,0.25,0.03,0.005,0.001]]]
+winnow_parameters = [[[0],[1.1,1.01,1.005,1.0005,1.0001]], [[2.0,0.3,0.04,0.006,0.001], [1.1,1.01,1.005,1.0005,1.0001]]]
+count_mistakes_files = {
+    500: 'dataset-500.libsvm',
+    1000: 'dataset-1000.libsvm'
+}
+num_rounds = 20
+instance_intervals = [i * 100 for i in range(1, 501)]
 
-def count_mistakes_winnow (n_val, dataset, training, testing):
-    winnow_params = winnow_get_params (n_val, training, testing)
-    winnow_params_without_margin = winnow_params.get_params (0)
-    winnow_params_with_margin = winnow_params.get_params (1)
-    
-    mistakes_without_margin = winnow_mistake_count (dataset,
-                                winnow_params_without_margin)
-    mistakes_with_margin = winnow_mistake_count (dataset,
-                                winnow_params_with_margin)
-    
-    return (mistakes_without_margin, mistakes_with_margin)
-    
+class PerceptronParams:
+    def __init__ (self):
+        self.weight_vector = []
+        self.bias = 0.0
+        self.gamma = 0.0
+        self.eta = 0.0
+        self.mistakes = 0
+        self.accuracy = 0.0
 
-def count_mistakes(n_val):
-    dataset = file_read (count_mistakes_datasets[n_val])
+class WinnowParams:
+    def __init__(self):
+        self.weight_vector = []
+        self.gamma = 0.0
+        self.eta = 0.0
+        self.mistakes = 0
+        self.accuracy = 0.0
+
+def active_to_index (active):
+    return int(active.split(':')[0])
+
+def perceptron_hypothesis_result (instance_vector, weight_vector, bias):
+    cur_sum = bias
+    for active in instance_vector:
+        index = active_to_index (active)
+        cur_sum += weight_vector[index]
+    
+    return cur_sum
+
+def perceptron_update_weights (instance_vector, actual, weight_vector, learning_rate):
+    for active in instance_vector:
+        index = active_to_index (active)
+        weight_vector[index] += actual * learning_rate
+    
+    return weight_vector
+
+def winnow_hypothesis_result (instance_vector, weight_vector, threshold):
+    cur_sum = -threshold
+    for active in instance_vector:
+        index = active_to_index (active)
+        cur_sum += weight_vector[index]
+    
+    return cur_sum
+
+def perceptron_update_bias (bias, actual, learning_rate):
+    return bias + actual * learning_rate
+
+def winnow_update_weights (instance_vector, actual, weight_vector, learning_rate):
+    for active in instance_vector:
+        index = active_to_index (active)
+        weight_vector[index] *= pow (learning_rate, actual)
+
+    return weight_vector
+
+def winnow_train (dimensions, training_set, margin, learning_rate):
+    weight_vector = [1.0] * (dimensions + 1)
+    cur_round = 0
+    threshold = dimensions / 2.0
+    
+    while cur_round < num_rounds:
+        for instance in training_set:
+            outcome = 1 if instance[0] == '+1' else -1
+            prediction = winnow_hypothesis_result (instance[1:], weight_vector, threshold)
+            if outcome * prediction <= margin:
+                weight_vector = winnow_update_weights (instance[1:], outcome, weight_vector, learning_rate)
+        cur_round += 1
+    
+    return weight_vector
+
+def winnow_test (testing_set, weight_vector, instance_intervals):
+    cumulative_mistakes = []
+    mistakes_so_far = 0
+    threshold = len (weight_vector) / 2.0
+    
+    for i in range(len(testing_set)):
+        instance = testing_set[i]
+        outcome = 1 if instance[0] == '+1' else -1
+        prediction = winnow_hypothesis_result (instance[1:], weight_vector, threshold)
+        prediction_class = 1 if prediction >= 0 else -1
+        if outcome != prediction_class:
+            mistakes_so_far += 1
+        if (i + 1) in instance_intervals:
+            cumulative_mistakes.append (mistakes_so_far)
+        
+    return cumulative_mistakes
+
+def perceptron_train (dimensions, training_set, margin, learning_rate):
+    weight_vector = [0.0] * (dimensions + 1)
+    bias = 0.0
+    cur_round = 0
+    
+    while cur_round < num_rounds:
+        for instance in training_set:
+            outcome = 1 if instance[0] == '+1' else -1
+            prediction = perceptron_hypothesis_result (instance[1:], weight_vector, bias)
+            if outcome * prediction <= margin:
+                weight_vector = perceptron_update_weights (instance[1:], outcome, weight_vector, learning_rate)
+                bias = perceptron_update_bias (bias, outcome, learning_rate)
+        cur_round += 1
+    
+    return [weight_vector, bias]
+
+def perceptron_test (testing_set, weight_vector, bias, instance_intervals):
+    cumulative_mistakes = []
+    mistakes_so_far = 0
+    
+    for i in range (len(testing_set)):
+        instance = testing_set[i]
+        outcome = 1 if instance[0] == '+1' else -1
+        prediction = perceptron_hypothesis_result (instance[1:], weight_vector, bias)
+        prediction_class = 1 if prediction >= 0 else -1
+        if outcome != prediction_class:
+            mistakes_so_far += 1
+        if (i + 1) in instance_intervals:
+            cumulative_mistakes.append (mistakes_so_far)
+    
+    return cumulative_mistakes
+
+def perceptron_generate_best_hyperparams (dimensions, training_set, testing_set, is_with_margin):
+    margin = 0 if is_with_margin is False else 1
+    gamma_range = perceptron_parameters[margin][0]
+    eta_range = perceptron_parameters[margin][1]
+    
+    cur_best_mistakes = float('inf')
+    best_hyperparams = PerceptronParams ()
+    
+    for gamma in gamma_range:
+        for eta in eta_range:
+            print 'currently training model with => gamma: ' + str(gamma) + ' eta: ' + str(eta)
+            training_result = perceptron_train (dimensions, training_set, gamma, eta)
+            weight_vector = training_result[0]
+            bias = training_result[1]
+            print 'training complete...testing on D2'
+            cumulative_mistakes = perceptron_test (testing_set, weight_vector, bias, [len(testing_set)])
+            if cumulative_mistakes[0] >= cur_best_mistakes:
+                print 'no improvement in accuracy, ignoring current gamma/eta settings...'
+            else:
+                print 'improved accuracy, updating current gamma/eta settings...'
+                cur_best_mistakes = cumulative_mistakes[0]
+                best_hyperparams.weight_vector = list(weight_vector)
+                best_hyperparams.bias = bias
+                best_hyperparams.gamma = gamma
+                best_hyperparams.eta = eta
+                best_hyperparams.mistakes = cur_best_mistakes
+                best_hyperparams.accuracy = 1.0 - float(cur_best_mistakes) / len(testing_set)
+    
+    print '\ncompleted running current algorithm, following are the findings: '
+    print 'best gamma: ' + str(best_hyperparams.gamma)
+    print 'best eta: ' + str(best_hyperparams.eta)
+    print 'mistakes made (D2): ' + str(best_hyperparams.mistakes)
+    print 'accuracy (D2): ' + str(best_hyperparams.accuracy)
+    
+    return best_hyperparams
+
+def winnow_generate_best_hyperparams (dimensions, training_set, testing_set, is_with_margin):
+    margin = 0 if is_with_margin is False else 1
+    gamma_range = winnow_parameters[margin][0]
+    eta_range = winnow_parameters[margin][1]
+    
+    cur_best_mistakes = float('inf')
+    best_hyperparams = WinnowParams ()
+    
+    for gamma in gamma_range:
+        for eta in eta_range:
+            print 'currently training model with => gamma: ' + str(gamma) + ' eta: ' + str(eta)
+            weight_vector = winnow_train (dimensions, training_set, gamma, eta)
+            print 'training complete...testing on D2'
+            cumulative_mistakes = winnow_test (testing_set, weight_vector, [len(testing_set)])
+            if cumulative_mistakes[0] >= cur_best_mistakes:
+                print 'no improvement in accuracy, ignoring current gamma/eta settings...'
+            else:
+                print 'improved accuracy, updating current gamma/eta settings...'
+                cur_best_mistakes = cumulative_mistakes[0]
+                best_hyperparams.weight_vector = list(weight_vector)
+                best_hyperparams.gamma = gamma
+                best_hyperparams.eta = eta
+                best_hyperparams.mistakes = cur_best_mistakes
+                best_hyperparams.accuracy = 1.0 - float(cur_best_mistakes) / len(testing_set)
+    
+    print '\ncompleted running current algorithm, following are the findings: '
+    print 'best gamma: ' + str(best_hyperparams.gamma)
+    print 'best eta: ' + str(best_hyperparams.eta)
+    print 'mistakes made (D2): ' + str(best_hyperparams.mistakes)
+    print 'accuracy (D2): ' + str(best_hyperparams.accuracy)
+    
+    return best_hyperparams
+            
+def generate_best_hyperparams (dimensions, training_set, testing_set):
+    hyperparams = [None] * 4
+    
+    print '\ncurrent algorithm: perceptron without margin'
+    hyperparams[0] = perceptron_generate_best_hyperparams (dimensions, training_set, testing_set, False)
+    print '\ncurrent algorithm: perceptron with margin'
+    hyperparams[1] = perceptron_generate_best_hyperparams (dimensions, training_set, testing_set, True)
+    print '\ncurrent algorithm: winnow without margin'
+    hyperparams[2] = winnow_generate_best_hyperparams (dimensions, training_set, testing_set, False)
+    print '\ncurrent algorithm: winnow with margin'
+    hyperparams[3] = winnow_generate_best_hyperparams (dimensions, training_set, testing_set, True)
+    
+    return hyperparams
+
+def plot_cumulative_mistakes_versus_instances (p_mistakes_no_margin, p_mistakes_margin, w_mistakes_no_margin, w_mistakes_margin):
+    plt.plot(instance_intervals, p_mistakes_no_margin, 'r-', label='perceptron without margin')
+    plt.plot(instance_intervals, p_mistakes_margin, 'b--', label='perceptron with margin')
+    plt.plot(instance_intervals, w_mistakes_no_margin, 'g-.', label='winnow without margin')
+    plt.plot(instance_intervals, w_mistakes_margin, 'y:', label='winnow with margin')
+    plt.xlabel('number of instances')
+    plt.ylabel('number of mistakes') 
+    plt.legend(loc='upper left')
+    plt.savefig('mistakes-versus-instances-'+ str(n_val) + '.png')
+
+def main ():
+    print 'n = ' + str(n_val)
+    libsvm_filename = count_mistakes_files[n_val]
+    ##################### Generate D1,D2 from libsvm #############################
+    dataset = file_read (libsvm_filename)
     partition = partition_data (dataset)
-    retVal1 = count_mistakes_perceptron (n_val, dataset, partition.training, partition.testing)
-    retVal2 = count_mistakes_winnow (n_val, dataset, partition.training, partition.testing)
-    plot_mistakes(retVal1[0],retVal2[1],retVal2[0],retVal2[1])
-                              
-def main():
-    # n = 500
-    # count_mistakes(0)
-    # n = 1000
-    count_mistakes(1)
+    training_set = partition.training
+    testing_set = partition.testing
+    
+    ######## Learn best performing hyperparameters for all four algorithms #######
+    best_hyperparams = generate_best_hyperparams (n_val, training_set, testing_set)
+    best_hyperparams_perc = best_hyperparams[0]
+    best_hyperparams_perc_margin = best_hyperparams[1]
+    best_hyperparams_winnow = best_hyperparams[2]
+    best_hyperparams_winnow_margin = best_hyperparams[3]
+    
+    ######## Count cumulative mistakes for all four algorithms on 50K dataset ####
+    print '\ncounting cumulative mistakes on N = 50,000 dataset...'
+    print 'current algorithm: perceptron without margin'
+    cumulative_mistakes_perc = perceptron_test (dataset, best_hyperparams_perc.weight_vector, best_hyperparams_perc.bias, instance_intervals)
+    print 'mistakes made, M: ' + str(cumulative_mistakes_perc[len(cumulative_mistakes_perc) - 1])
+    print 'current algorithm: perceptron with margin'
+    cumulative_mistakes_perc_margin = perceptron_test (dataset, best_hyperparams_perc_margin.weight_vector, best_hyperparams_perc_margin.bias, instance_intervals)
+    print 'mistakes made, M: ' + str(cumulative_mistakes_perc[len(cumulative_mistakes_perc) - 1])
+    print 'current algorithm: winnow without margin'
+    cumulative_mistakes_winnow = winnow_test (dataset, best_hyperparams_winnow.weight_vector, instance_intervals)
+    print 'mistakes made, M: ' + str(cumulative_mistakes_perc[len(cumulative_mistakes_perc) - 1])
+    print 'current algorithm: winnow with margin'
+    cumulative_mistakes_winnow_margin = winnow_test (dataset, best_hyperparams_winnow_margin.weight_vector, instance_intervals)
+    print 'mistakes made, M: ' + str(cumulative_mistakes_perc[len(cumulative_mistakes_perc) - 1])
+    ################ Plot mistakes versus instances for current n value ##########
+    plot_cumulative_mistakes_versus_instances (cumulative_mistakes_perc, cumulative_mistakes_perc_margin, cumulative_mistakes_winnow, cumulative_mistakes_winnow_margin)
+    
+    
 
 if __name__ == '__main__':
-    main()
-
-
-'''
-Findings: 
-
-winnow(with margin)
-n = 500; gamma = 2.0, eta = 1.1, mistakes(50,000) = 741, mistakes(5,000) = 88
-n = 1000; gamma = 0.3, eta = 1.1, mistakes(50,000) = 928, mistakes(5,000) = 88
-
-winnow(without margin)
-n = 500; gamma = 0, eta = 1.1, mistakes(50,000) = 896, mistakes(5,000) = 97
-n = 1000; gamma = 0, eta = 1.1, mistakes(50,000) = 1053, mistakes(5,000) = 94
-
-perceptron(with margin)
-n = 500; gamma = 1, eta = 0.001, mistakes(50,000) = 1030, mistakes(5,000) = 126
-n = 1000; gamma = 1, eta = 0.005, mistakes(50,000) = 1387, mistakes(5,000) = 138
-
-perceptron(without margin)
-n = 500; gamma = 0, eta = 1, mistakes(50,000) = 1185, mistakes(5,000) = 135
-n = 1000; gamma = 0, eta = 1, mistakes(50,000) = 1462, mistakes(5,000) = 146
-'''
+    main ()
--- Perceptron/learning_curves.py
+++ Perceptron/learning_curves.py
@@ -1,57 +1,121 @@
 #!/usr/bin/python
-from constants import learning_curves_datasets, learning_curves_dimensions
 from datahelper import file_read, partition_data
-from perceptron import perceptron_get_params, perceptron_mistakes_survive
-from winnow import winnow_get_params, winnow_mistakes_survive
-from plothelper import plot_learning_curves
+from count_mistakes import perceptron_generate_best_hyperparams, winnow_generate_best_hyperparams
+from count_mistakes import perceptron_hypothesis_result, winnow_hypothesis_result
+from count_mistakes import perceptron_update_weights, perceptron_update_bias, winnow_update_weights
+from constants import num_rounds
+import matplotlib.pyplot as plt
 
-def learn_curves_perceptron (n_val, dataset, training, testing):
-    dimensions = learning_curves_dimensions[n_val]
-    perceptron_params = perceptron_get_params (dimensions, training, testing)
-    
-    perceptron_params_without_margin = perceptron_params.get_params(0)
-    perceptron_params_with_margin = perceptron_params.get_params(1)
-    
-    mistakes_without_margin = perceptron_mistakes_survive (dataset, 
-                                perceptron_params_without_margin)
-    
-    return mistakes_without_margin
 
-def learn_curves_winnow (n_val, dataset, training, testing):
-    dimensions = learning_curves_dimensions[n_val]
-    winnow_params = winnow_get_params (dimensions, training, testing)
-    
-    winnow_params_without_margin = winnow_params.get_params (0)
-    winnow_params_with_margin = winnow_params.get_params (1)
-    
-    mistakes_with_margin = winnow_mistakes_survive (dataset, 
-                                winnow_params_with_margin)
-    
-    return mistakes_with_margin
+n_vals = [40, 80, 120, 160, 200]
+halting_instances = 700
+learning_curves_files = {
+    40: 'dataset-40.libsvm',
+    80: 'dataset-80.libsvm',
+    120: 'dataset-120.libsvm',
+    160: 'dataset-160.libsvm',
+    200: 'dataset-200.libsvm'
+}
 
-def learn_curves (n_val):
-    dataset = file_read (learning_curves_datasets[n_val])
-    partition = partition_data (dataset)
-    #return learn_curves_perceptron (n_val, dataset, partition.training, partition.testing)
-    return learn_curves_winnow (n_val, dataset, partition.training, partition.testing)
+def does_winnow_converge (dataset, dimensions, margin, learning_rate, mistakes, index):
+    weight_vector = [1.0] * (dimensions + 1)
+    threshold = dimensions / 2.0
+    cur_round = 0
+    
+    while cur_round < num_rounds:
+        print 'cur round: ' + str(cur_round + 1)
+        instances_survived = 0
+        mistakes_made = 0
+        for data_row in dataset:
+            outcome = 1 if data_row[0] == '+1' else -1
+            prediction = winnow_hypothesis_result (data_row[1:], weight_vector, threshold)
+            if outcome * prediction > margin:
+                instances_survived += 1
+                if instances_survived >= halting_instances:
+                    mistakes[index] = mistakes_made
+                    return True
+            else:
+                weight_vector = winnow_update_weights (data_row[1:], outcome, weight_vector, learning_rate)
+                mistakes_made += 1
+                instances_survived = 0
+                
+        print 'total mistakes at end of round: ' + str(mistakes_made)
+        cur_round += 1
+
+    return False
+
+def does_perceptron_converge (dataset, dimensions, margin, learning_rate, mistakes, index):
+    weight_vector = [0.0] * (dimensions + 1)
+    bias = 0.0
+    cur_round = 0
+    
+    while cur_round < num_rounds:
+        print 'cur round: ' + str(cur_round + 1)
+        instances_survived = 0
+        mistakes_made = 0
+        for data_row in dataset:
+            outcome = 1 if data_row[0] == '+1' else -1
+            prediction = perceptron_hypothesis_result (data_row[1:], weight_vector, bias)
+            if outcome * prediction > margin:
+                instances_survived += 1
+                if instances_survived >= halting_instances:
+                    mistakes[index] = mistakes_made
+                    return True
+            else:
+                weight_vector = perceptron_update_weights (data_row[1:], outcome, weight_vector, learning_rate)
+                bias = perceptron_update_bias (bias, outcome, learning_rate)
+                mistakes_made += 1
+                instances_survived = 0
+        
+        print 'total mistakes at end of round: ' + str(mistakes_made)
+        cur_round += 1
+    
+    return False
+
+def plot_mistakes_versus_n ():
+    winnow_with_margin_mistakes = [435, 253, 489, 516, 472]
+    winnow_mistakes = [169, 349, 376, 466, 472]
+    perceptron_with_margin_mistakes = [188, 677, 377, 194, 462]
+    perceptron_mistakes = [456, 530, 377, 194, 577]
+    plt.plot(n_vals, winnow_mistakes, 'g-.', label='winnow without margin')
+    plt.plot(n_vals, winnow_with_margin_mistakes, 'y:', label='winnow with margin')
+    plt.plot(n_vals, perceptron_mistakes, 'r-', label='perceptron without margin')
+    plt.plot(n_vals, perceptron_with_margin_mistakes, 'b--', label='perceptron with margin')
+    plt.xlabel('n')
+    plt.ylabel('mistakes') 
+    plt.legend(loc='upper right')
+    plt.savefig('mistakes-versus-n.pdf')
 
 def main ():
-    plot_learning_curves([
-        # n = 40
-        learn_curves (0),
-        # n = 80
-        learn_curves (1),
-        # n = 120
-        learn_curves (2),
-        # n = 160
-        learn_curves (3),
-        # n = 200
-        learn_curves (4)
-        ],
-        learning_curves_dimensions,
-        'winnow_with_margin_lc',
-        'winnow with margin'
-    )
+    mistakes_at_halting = [0] * len(n_vals)
+    index = 0
+    is_success = True
+    '''
+    for n_val in n_vals:
+        print '\nn = ' + str(n_val)
+        libsvm_filename = learning_curves_files[n_val]  
+        ##################### Generate D1,D2 from libsvm #############################
+        dataset = file_read (libsvm_filename)
+        partition = partition_data (dataset)
+        training_set = partition.training
+        testing_set = partition.testing
+        
+        ######## Learn best performing hyperparams for current algorithm #############
+        best_hyperparams = winnow_generate_best_hyperparams (n_val, training_set, testing_set, True)
+        
+        ######## Count cumulative mistakes for current n_val, and some value of S ####
+        print '\nmeasuring convergence with S = ' + str(halting_instances)
+        if does_winnow_converge (dataset, n_val, best_hyperparams.gamma, best_hyperparams.eta, mistakes_at_halting, index):
+            print 'winnow converges for given S, mistakes made at halt: ' + str(mistakes_at_halting[index])
+            index += 1
+        else:
+            is_success = False
+            print 'winnow does not survive given S, try with a new S'
+            break
+    '''
+    if is_success is True:
+        plot_mistakes_versus_n ()
+    
 
 if __name__ == '__main__':
     main ()
